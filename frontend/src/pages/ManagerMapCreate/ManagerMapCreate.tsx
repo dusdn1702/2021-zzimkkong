a50c48c53291d54dc8e672eb7c7d428611a17c3f
yujo <yujo.dev@gmail.com>
2021-08-05 22:25:33 +0900

    refactor: 마우스 포인터 사이즈 및 색상 조절
    - r : 4- > 3
    - fill : PALETTE.OPACITY_BLACK[700] -> PALETTE.OPACITY_BLACK[300]

--- frontend/src/pages/ManagerMapCreate/ManagerMapCreate.tsx
+++ frontend/src/pages/ManagerMapCreate/ManagerMapCreate.tsx
@@ -415,8 +415,8 @@ const ManagerMapCreate = (): JSX.Element => {
                     <circle
                       cx={stickyCoordinate.x}
                       cy={stickyCoordinate.y}
-                      r={4}
-                      fill={PALETTE.OPACITY_BLACK[700]}
+                      r={3}
+                      fill={PALETTE.OPACITY_BLACK[300]}
                     />
                     {mapElements.map((element) => (
                       <polyline
